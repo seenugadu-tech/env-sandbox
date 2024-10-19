b79876a09793c71f3088db5cb8f6e368cecd4a8f
seenu <srinivas.ram7@yahoo.com>
2024-10-19 19:26:43 -0400

    updated main

--- main.tf
+++ main.tf
@@ -13,6 +13,19 @@ locals {
 }
 
 # Network Configuration
+module "aws-network" {
+  source = "github.com/seenugadu-tech/module-aws-network"
+
+  env_name              = local.env_name
+  vpc_name              = "msur-VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
 
 # EKS Configuration
 
