864f0cc09a4a5fb9e31131f782b72567c9e3e7e4
seenu <srinivas.ram7@yahoo.com>
2024-10-20 08:33:47 -0400

    initial ArgoCD

--- main.tf
+++ main.tf
@@ -48,3 +48,13 @@ module "aws-kubernetes-cluster" {
 }
 
 # GitOps Configuration
+module "argo-cd-server" {
+  source = "github.com/seenugadu-tech/module-argo-cd"
+
+  kubernetes_cluster_id        = module.aws-eks.eks_cluster_id
+  kubernetes_cluster_name      = module.aws-eks.eks_cluster_name
+  kubernetes_cluster_cert_data = module.aws-eks.eks_cluster_certificate_data
+  kubernetes_cluster_endpoint  = module.aws-eks.eks_cluster_endpoint
+  eks_nodegroup_id             = module.aws-eks.eks_cluster_nodegroup_id
+
+}
